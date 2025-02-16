aae54e82254cc78a76c7287ade35f71cfe01ac0b
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2025-02-16 15:15:40 +0200

    some metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Prière du Jour',
-  description: '',
-  keywords:["Prière","Prière du Jour", "Guide du Jour"]
+  description: 'Une courte prière du jour pour nourrir votre foi et trouver la paix intérieure. Un moment de recueillement quotidien pour vous accompagner spirituellement.',
+  keywords:["Prière","Prière du Jour", "Guide du Jour", "Sel du monde"]
 };
 
 export default function RootLayout({
